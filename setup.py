d966adbf98156ab935ba2c5406353ff2d76ef99c
Dmitry Vasilyev <dispelpowerone@gmail.com>
2023-08-26 17:21:30 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='driver_test_db',
+    version='1.0',
+    description='Utils to build driver test database',
+    author='Dmitry Vasilyev',
+    author_email='dispelpowerone@gmail.com',
+    packages=['driver_test_db'],
+)
