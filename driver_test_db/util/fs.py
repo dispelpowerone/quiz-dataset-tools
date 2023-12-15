1cc443a4592e944c30454090799a5d53a6286e07
Dmitry Vasilyev <dispelpowerone@gmail.com>
2023-12-14 17:39:56 -0800

    Fix files order in load_list function

--- driver_test_db/util/fs.py
+++ driver_test_db/util/fs.py
@@ -34,9 +34,27 @@ def dump_list(cls: Type[T], data: list[T], output_dir: str, chunk_size: int) -> 
 def load_list(cls: Type[T], source_dir: str) -> list[T]:
     if not os.path.isdir(source_dir):
         raise Exception(f"No source directory found {source_dir}")
+    file_paths = list_partial_files(f"{source_dir}/out.__INDEX__.json")
     data = []
-    file_paths = glob.glob(f"{source_dir}/out.*.json")
     for file_path in file_paths:
         with open(file_path, "r") as fd:
             data.extend(cls.schema().loads(fd.read(), many=True))
     return data
+
+
+def list_partial_files(template: str) -> list[str]:
+    placeholder = "__INDEX__"
+    # Build list of files
+    index = 1
+    files_list: list[str] = []
+    while True:
+        file_name = template.replace(placeholder, str(index))
+        if not os.path.isfile(file_name):
+            break
+        files_list.append(file_name)
+        index += 1
+    # Check the list
+    glob_paths = glob.glob(template.replace(placeholder, "*"))
+    if len(files_list) != len(glob_paths):
+        raise Exception(f"Broken files list: {files_list} != {glob_paths}")
+    return files_list
