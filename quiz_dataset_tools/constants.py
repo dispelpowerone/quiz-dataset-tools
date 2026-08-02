52a41e1a8979bde93191e13edcdd6797e21c8e86
Dmitry Vasilyev <dispelpowerone@gmail.com>
2026-08-02 08:30:31 +0000

    Switch to gpt terra model + add TX const

--- quiz_dataset_tools/constants.py
+++ quiz_dataset_tools/constants.py
@@ -2,12 +2,15 @@ DOMAIN_TEST_TYPE = {
     "on": "G1 Driving Test Ontario",
     "ny": "New York DMV written test",
     "fl": "Florida Learner's Permit Test",
+    "tx": "Texas Learner's Permit Test",
 }
 
 DOMAIN_UNITS_SYSTEM = {
     "on": "metric system",
     "ny": "imperial system",
     "fl": "imperial system",
+    "tx": "imperial system",
 }
 
-GPT_MODEL = "gpt-4o"
+# GPT_MODEL = "gpt-4o"
+GPT_MODEL = "gpt-5.6-terra"
